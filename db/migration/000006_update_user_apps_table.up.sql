33d6f9e4e5c7464d4367e5013401121074b1f1de
oayomide <onigbindeayomide@gmail.com>
2025-01-04 21:10:41 +0100

    chore(migration): combine all migrations into one

--- db/migration/000006_update_user_apps_table.up.sql
+++
@@ -1,9 +0,0 @@
-alter table if exists user_apps
-    add id integer generated always as identity;
-
-alter table if exists user_apps
-    add username varchar;
-
-alter table if exists user_apps
-    add constraint user_apps_pk
-        primary key (id);
--- db/migration/000008_update_app_column_date_timestamps.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE IF EXISTS apps
-    ALTER COLUMN created_at TYPE pg_catalog.date USING created_at::pg_catalog.date;
-
-ALTER TABLE IF EXISTS apps
-    ALTER COLUMN updated_at TYPE pg_catalog.date USING updated_at::pg_catalog.date;
